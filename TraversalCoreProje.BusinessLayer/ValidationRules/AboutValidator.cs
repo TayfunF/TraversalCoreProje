b7323242bd7dbb038aedebf12ecb1f1f31729c16
TayfunF <tayfunbjk08@hotmail.com>
2023-05-07 13:28:35 +0300

    FluentValidation Library Added

---
+++ TraversalCoreProje.BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalCoreProje.EntityLayer.Concrete;
+
+namespace TraversalCoreProje.BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama alanı boş bırakılamaz");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Lütfen görsel seçiniz");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen en az 50 karakterlik açıklama giriniz");
+            RuleFor(x => x.Description).MinimumLength(1500).WithMessage("Lütfen en fazla 1500 karakterlik açıklama giriniz");
+        }
+    }
+}
