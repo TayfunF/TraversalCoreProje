72380e229ec59fb3cf9edd547cb64468dadde28f
TayfunF <tayfunbjk08@hotmail.com>
2023-05-07 13:20:38 +0300

    BusinessLayer için Abstract ve Conctre klasörleri oluşturuldu.

---
+++ TraversalCoreProje.BusinessLayer/Abstracts/IDestinationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalCoreProje.EntityLayer.Concrete;
+
+namespace TraversalCoreProje.BusinessLayer.Abstracts
+{
+    public interface IDestinationService : IGenericService<Destination>
+    {
+    }
+}
---
+++ TraversalCoreProje.BusinessLayer/Concretes/AboutService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraversalCoreProje.BusinessLayer.Abstracts;
+using TraversalCoreProje.DataAccessLayer.Abstract;
+using TraversalCoreProje.EntityLayer.Concrete;
+
+namespace TraversalCoreProje.BusinessLayer.Concretes
+{
+    public class AboutService : IAboutService
+    {
+        IAboutRepository _aboutRepository;
+
+        public AboutService(IAboutRepository aboutRepository)
+        {
+            _aboutRepository = aboutRepository;
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutRepository.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutRepository.Delete(t);
+        }
+
+        public About TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutRepository.GetList();
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutRepository.Update(t);
+        }
+    }
+}
