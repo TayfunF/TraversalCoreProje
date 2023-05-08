bd5429209414b1862247b69866c66063a64e39df
TayfunF <tayfunbjk08@hotmail.com>
2023-05-08 16:02:47 +0300

    UI ve LAyout Düzenlemeleri yapıldı.

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.BusinessLayer.Concretes;
+using TraversalCoreProje.DataAccessLayer.Repositories;
+using TraversalCoreProje.EntityLayer.Concrete;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationService _destinationService = new DestinationService(new DestinationRepository());
+        public IActionResult Index()
+        {
+            return View(_destinationService.TGetList());
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+
+        public IActionResult DestinationDetails(Destination destination)
+        {
+            return View();
+        }
+    }
+}
