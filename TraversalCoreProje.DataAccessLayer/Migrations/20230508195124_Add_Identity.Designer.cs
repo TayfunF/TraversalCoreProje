0dad011cf697521257a027e7e7077827a1ebd273
TayfunF <tayfunbjk08@hotmail.com>
2023-05-08 23:26:20 +0300

    Register ve Login Templateler Eklendi. Controller oluşturuldu

--- TraversalCoreProje.DataAccessLayer/Migrations/20230508195124_Add_Identity.Designer.cs
+++ TraversalCoreProje.DataAccessLayer/Migrations/20230508195124_Add_Identity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TraversalCoreProje.DataAccessLayer;
 
 namespace TraversalCoreProje.DataAccessLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230508195124_Add_Identity")]
+    partial class Add_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
