d6b84c2e40edd2294357e6a54dcc8561b832a023
TayfunF <tayfunbjk08@hotmail.com>
2023-05-09 16:51:54 +0300

    Dashboard teması eklendi. ÜyePaneli için Partiallar oluşturuldu.

---
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.BusinessLayer;
+using TraversalCoreProje.DataAccessLayer;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationService _destinationService = new DestinationService(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            return View(_destinationService.TGetList());
+        }
+    }
+}
