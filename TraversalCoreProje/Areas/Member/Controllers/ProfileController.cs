292290e6f2c8e4fdaa24df0314d3675183cda96d
TayfunF <tayfunbjk08@hotmail.com>
2023-05-10 19:44:01 +0300

    #36 Identity ile Profil & Resim Bilgisi Güncelleme

--- TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using TraversalCoreProje.Areas.Member.Models;
 using TraversalCoreProje.EntityLayer;
@@ -22,16 +24,46 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            
+
             UserEditVM userEditVM = new UserEditVM();
             userEditVM.Name = values.Name;
             userEditVM.Surname = values.Surname;
+            userEditVM.UserName = values.UserName;
             userEditVM.Email = values.Email;
-            userEditVM.Phone = values.PhoneNumber;
+            userEditVM.PhoneNumber = values.PhoneNumber;
 
             return View(userEditVM);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditVM userEditVM)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
 
+            if (userEditVM.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(userEditVM.Image.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/userimages/" + imageName;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await userEditVM.Image.CopyToAsync(stream);
+                user.ImageUrl = "/userimages/" + imageName;
+            }
+
+            user.Name = userEditVM.Name;
+            user.Surname = userEditVM.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditVM.Password);
+            user.PhoneNumber = userEditVM.PhoneNumber;
+            user.Email = userEditVM.Email;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+
+            return View();
+        }
     }
 }
